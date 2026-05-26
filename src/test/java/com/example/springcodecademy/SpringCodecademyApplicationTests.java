3019c8799ae52b409d3f24cdda68dd2f6a8de4be
Gabriel Paniagua <gabrielpan295@gmail.com>
2026-05-26 10:58:56 -0500

    test: add Spring context load test
    Verifies the application context starts up without errors.

---
+++ src/test/java/com/example/springcodecademy/SpringCodecademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcodecademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCodecademyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
