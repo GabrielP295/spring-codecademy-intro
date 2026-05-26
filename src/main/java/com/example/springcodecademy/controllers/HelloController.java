f18709ee0eb55f87fe539c7d1243a041a04327f3
Gabriel Paniagua <gabrielpan295@gmail.com>
2026-05-26 10:59:01 -0500

    feat: add hello world REST endpoint
    Adds GET /helloworld via @RestController and @GetMapping.
    First hands-on use of Spring MVC's annotation-driven routing.

---
+++ src/main/java/com/example/springcodecademy/controllers/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.springcodecademy.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/helloworld")
+    public String getHello() {
+        return "Hello World!!!!! the skies the limit!!!!";
+    }
+}
